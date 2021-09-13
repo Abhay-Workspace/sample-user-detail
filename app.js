15943c495c2922c19a8489b43dde277e7cf58a1d
Abhay-Workspace <work.abhaypaswan@gmail.com>
2021-09-13 23:01:00 +0530

    added sample profile pic

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ app.get("/user1", (req, res, next) => {
         "name": 'Simon Baker', 
         'tournaments_played': 34, 
         'tournaments_won': 09,
-        'win_per': 26,
+        'win_per': 26, 
+        'image_url': 'https://img5.goodfon.com/wallpaper/nbig/7/d0/vzgliad-kostium-akter-saimon-beiker-simon-baker.jpg',
     });
 });
 
@@ -25,5 +26,6 @@ app.get("/user2", (req, res, next) => {
         'tournaments_played': 20, 
         'tournaments_won': 01,
         'win_per': 05,
+        'image_url': 'https://t4.ftcdn.net/jpg/02/97/28/87/240_F_297288719_dMUUuJK6lRgdNzRuQE5ESEUojANdNqSq.jpg',
     });
 });
